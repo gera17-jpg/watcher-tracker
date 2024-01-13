3b1ada4f67e271d521841fac50686ccdc27c6e8c
gera17-jpg <utorrent212@gmail.com>
2024-01-13 21:04:05 +0800

    edited the codes

--- tracker.js
+++ tracker.js
@@ -1,31 +1,40 @@
 const list = [];
 
 function addData() {
-    const dateClass = document.querySelector('.dateClass');
-    const date = dateClass.value;
+    let dateInput = document.getElementsByClassName('dateClass')[0].value;
+    let dayInput = document.getElementsByClassName('dayClass')[0].value;
+    let nightInput = document.getElementsByClassName('nightClass')[0].value;
 
-    const watcherClasses = document.querySelectorAll('.watcherClass');
-    const watchers = Array.from(watcherClasses).map(watcher => watcher.value);
-
-    const data = { date, watchers };
+    let data={
+        date: dateInput,
+        dayWatcher: dayInput,
+        nightWatcher: nightInput
+    }
+     // Add the new data to the list array
     list.push(data);
+    console.log(data);
 
-    console.log(list); // Just for testing
+// Optionally, you might want to clear the input fields after saving data
+    document.getElementsByClassName('dateClass')[0].value = '';
+    document.getElementsByClassName('dayClass')[0].value = '';
+    document.getElementsByClassName('nightClass')[0].value = '';
 
-    // Optionally, you might want to clear the input fields after saving data
-    dateClass.value = '';
-    watcherClasses.forEach(watcher => (watcher.value = ''));
-
-    // You might want to call a function to display this data
+// You might want to call a function to display this data
     displayData();
 }
 
 function displayData() {
-    // This function should display the data in some way, such as updating the UI
-    // Loop through the 'list' array and update the UI to show the stored data
-    for (let i = 0; i < list.length; i++) {
-        // Access list[i] to update your UI
-        // For instance, create new DOM elements to display the data
-        // Example: Create <div> or <p> elements to show the stored 'date' and 'watchers'
-    }
+    // Assuming you want to display the latest data entered
+    let latestData = list[list.length - 1];
+
+    // Access the DOM elements where you want to display the data
+    let storedDateElement = document.getElementById('storedDate');
+    let storedDayWatcherElement = document.getElementById('storedDayWatcher');
+    let storedNightWatcherElement = document.getElementById('storedNightWatcher');
+
+    // Update the UI with the latest data
+    storedDateElement.innerHTML = latestData.date;
+    storedDayWatcherElement.innerHTML =latestData.dayWatcher;
+    storedNightWatcherElement.innerHTML =latestData.nightWatcher;
 }
+
